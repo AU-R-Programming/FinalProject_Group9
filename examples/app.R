d8bccee55991f84e5213d3ef4262ee879f24074c
Kerry <cobbkerry@gmail.com>
2020-12-04 11:24:08 -0600

    Removed unused code from R Shiny example

--- examples/app.R
+++ examples/app.R
@@ -6,30 +6,12 @@ ui <- fluidPage(
   )
 )
 
-#   pageWithSidebar(
-#
-#   # headerPanel('Group9LinearModel'),
-#
-#   # sidebarPanel(
-#     # numericInput('clusters', 'Cluster count', 3, min = 1, max = 9)
-#   # ),
-#
-#   mainPanel(
-#     plotOutput('plot')
-#   )
-# )
-
 server <- function(input, output, session) {
   library(MASS)
   library(Group9LinearModel)
   data(Boston)
 
   fit = myLm(Boston$crim, Boston[c("age", "medv")])
-
-
-  # df <- iris[, c("Sepal.Length", "Sepal.Width")]
-
-  # clusters <- reactive({kmeans(df, input$clusters)})
 
   output$plot <- renderPlot({
     plot(fit)
